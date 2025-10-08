0823a086e079fb6bdb903d51836b46b04bd50046
jerry-song6 <791259487@qq.com>
2025-10-08 17:46:22 +0800

    config:deploy

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,3 @@
-/*
- * @Author: Songjia3 Jia3.Song@cicc.com.cn
- * @Date: 2025-10-06 17:11:17
- * @LastEditors: Songjia3 Jia3.Song@cicc.com.cn
- * @LastEditTime: 2025-10-07 15:02:49
- * @Description: Do not edit
- */
-// vite.config.js
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
@@ -14,6 +6,15 @@ export default defineConfig({
   build: {
     outDir: "dist",
     sourcemap: false,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ["react", "react-dom"],
+          ocr: ["tesseract.js"],
+          excel: ["xlsx", "file-saver"],
+        },
+      },
+    },
   },
   server: {
     port: 3000,
